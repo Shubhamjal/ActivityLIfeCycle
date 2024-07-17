44ed920f6dbfa6959777f126add96fc2ea3b700b
shubhamKumar <appscicseshubham22@gmail.com>
2024-07-17 13:17:49 +0530

    done

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.activitylifecycle
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var btn:Button?= null// var button declaration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show()
+
+        btn=findViewById(R.id.button)
+        btn?.setOnClickListener {
+            Toast.makeText(this,"Button IS CLICKED",Toast.LENGTH_SHORT).show()
+            var Intent=Intent(this,SecondActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ app/src/main/java/com/example/activitylifecycle/SecondActivity.kt
@@ -0,0 +1,33 @@
+package com.example.activitylifecycle
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+
+    var btn: Button?= null// var button declaration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btn=findViewById(R.id.button2)
+        btn?.setOnClickListener {
+            Toast.makeText(this,"Button IS CLICKED", Toast.LENGTH_SHORT).show()
+            var Intent= Intent(this,MainActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+}
